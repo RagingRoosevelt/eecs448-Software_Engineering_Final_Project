6302835cc610c1d7794e57c98bc383fdba8b4d5c
citizenkoehn <pete.koehn@gmail.com>
2014-12-12 18:16:40 -0600

    Pete's versions of Controller and Model
    Added xml parsing with ElementTree API.

---
+++ codeMODEL_pete.py
@@ -0,0 +1,30 @@
+from xml.etree import ElementTree as ET
+tree = ET.parse('TestRecipe.xml')
+root = tree.getroot()
+
+# Get recipe data
+name = root[0].get('name')
+servings = root[0][0].text
+bakeTime = root[0][1][0].text
+timeUnits = root[0][1][1].text
+bakeTemp = root[0][2][0].text
+tempUnits = root[0][2][1].text
+procedure = root[0][4].text
+
+# Mx3 array of ingredients
+ingredients = []
+ingredients.append([])
+ingredients.append([])
+ingredients.append([])
+
+# The following strings may be used to access the arrays columns
+names = 0
+quantities = 1
+units = 2
+
+for ingredient in root[0][3].findall('ingredient'):
+    ingredients[names].append(ingredient.get('name'))
+    ingredients[quantities].append(ingredient.get('quantity'))
+    ingredients[units].append(ingredient.get('unit'))
+
+# There is likely a better way to implement the ingredient array. I'm open to suggestions!
