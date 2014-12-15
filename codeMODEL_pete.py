50cb9e17fd0fb636f02642e5043c818d1c013708
citizenkoehn <pete.koehn@gmail.com>
2014-12-15 16:38:26 -0600

    added full read and write for xml
    we can write with xml to a file using our 'list-of-lists' recipe
    structure. i'm no wiz with python, so feel free to correct anything you
    like!

--- codeMODEL_pete.py
+++ codeMODEL_pete.py
@@ -1,40 +1,97 @@
-def scanForRecipes(directory):
+## You can use these commands to test these functions:
+##    scanForRecipes()
+##    tree = getXML('TestRecipe.xml')
+##    recipe = readRecipe(tree)
+##    writeRecipe(filename, recipe)
 
-    import os, sys
+import os, sys
+from xml.etree import ElementTree as ET
+
+# Searches directory for .xml recipe files
+# Returns a list of strings of recipe filenames
+def scanForRecipes(directory = None):
+    
     recipeList = []
 
-    recipeList += [each for each in os.listdir(directory) if each.endswith('.xml')]
+    # If function is executed without arguments, use current working directory
+    if directory is None:
+        recipeList += [each for each in os.listdir(os.getcwd()) if each.endswith('.xml')]
+    else:
+        recipeList += [each for each in os.listdir(directory) if each.endswith('.xml')]
 
     return recipeList
 
-def readRecipe(string):
-    
-    from xml.etree import ElementTree as ET
+# Converts given .xml file into tree structure
+# Returns tree
+def getXML(string):
+
+    #from xml.etree import ElementTree as ET
     tree = ET.parse(string)
+
+    return tree
+
+# Converts tree structure into list of lists
+# Returns list of lists that represents the recipe
+def readRecipe(tree):
+
     root = tree.getroot()
 
-    # Get recipe data
-    name = root[0].get('name')
-    servings = root[0][0].text
-    bakeTime = root[0][1][0].text
-    timeUnits = root[0][1][1].text
-    bakeTemp = root[0][2][0].text
-    tempUnits = root[0][2][1].text
-    procedure = root[0][4].text
-
-    # Mx3 array of ingredients
+    # Array for ingredients
     ingredients = []
-    ingredients.append([])
-    ingredients.append([])
-    ingredients.append([])
+    i = 0
 
-    # The following strings may be used to access the arrays columns
-    names = 0
-    quantities = 1
-    units = 2
-
-    # There is likely a better way to implement the ingredient array. I'm open to suggestions!
     for ingredient in root[0][3].findall('ingredient'):
-        ingredients[names].append(ingredient.get('name'))
-        ingredients[quantities].append(ingredient.get('quantity'))
-        ingredients[units].append(ingredient.get('unit'))
+        ingredients.append([])
+        ingredients[i].append(ingredient.get('name'))
+        ingredients[i].append(ingredient.get('quantity'))
+        ingredients[i].append(ingredient.get('unit'))
+        i += 1
+
+    recipe = []
+    recipe.append(root[0].get('name'))
+    recipe.append(root[0][0].text)
+    recipe.append([])
+    recipe[2].append(root[0][1][0].text)
+    recipe[2].append(root[0][1][1].text)
+    recipe.append([])
+    recipe[3].append(root[0][2][0].text)
+    recipe[3].append(root[0][2][1].text)
+    recipe.append(ingredients)
+    recipe.append(root[0][4].text)
+
+    return recipe
+
+# Converts list of lists to string and writes to file
+# Returns nothing
+def writeRecipe(filename, recipe):    
+    
+    file = open(filename, 'w')
+
+    file.write('<?xml version ="1.0"?>')
+
+    recipeString1 = '<data><recipe name="' + recipe[0] + '">'\
+                    '<servings>' + recipe[1] + '</servings>'\
+                    '<baketime><time>' + recipe[2][0] + '</time>'\
+                    '<unit>' + recipe[2][1] + '</unit></baketime>'\
+                    '<baketemp><temp>' + recipe [3][0] + '</temp>'\
+                    '<unit>' + recipe[3][1] + '</unit></baketemp>'
+
+    recipeString2 = '<ingredients>'
+
+    i = 0
+    
+    for ingred in recipe[4]:
+        recipeString2 += '<ingredient name="' + recipe[4][i][0] + \
+        '" quantity="' + recipe[4][i][1] + '" unit="' + recipe[4][i][2] + \
+        '"/>'
+        i += 1
+
+    recipeString2 += '</ingredients>'
+
+    recipeString3 = '<procedure>' + recipe[5] + '</procedure></recipe></data>'
+
+    recipeString = recipeString1 + recipeString2 + recipeString3
+    
+    file.write(recipeString)
+    
+    file.close()
